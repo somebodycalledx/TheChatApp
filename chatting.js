01b6dec8190aa4d0184fbc3a6b4e7af533f4af28
somebodycalledx <115419899+somebodycalledx@users.noreply.github.com>
2022-11-20 10:49:10 +0000

    Add files via upload

--- chatting.js
+++ chatting.js
@@ -13,10 +13,32 @@ const firebaseConfig = {
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
-    //Start code
+     row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+        document.getElementById("output").innerHTML += row;
 
     //End code
     });});}
 getData();
 
+function addRoom() {
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+})
 
+   localStorage.setItem("room_name", room_name)
+   window.location = "chatting.html"
+
+
+}
+
+function redirectToRoomName(name) {
+  localStorage.setItem("room_name", name);
+  window.location = "chatting.html";
+}
+
+function logout() {
+  localStorage.removeItem("user_name");
+  localStorage.removeItem(room_name); 
+  window.location = "login.html";
+}
